7934e403ec046bb3257b93961d32bae3770b7cfe
Yusuf Hendrawan <yusuf.hendrawan@tokopedia.com>
2019-10-11 16:59:14 +0700

    Add Kotlin Example

---
+++ kotlin/MathUtil/build.gradle.kts
@@ -0,0 +1,30 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.50"
+    id("maven-publish")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.example.math"
+            artifactId = "MathUtil"
+            version = "1.0"
+
+            from(components["java"])
+        }
+    }
+}
